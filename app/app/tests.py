b0bb1f365e30e82946e40de8712b75dd79cf414c
kylewagner17 <60904904+kylewagner17@users.noreply.github.com>
2025-06-03 11:39:28 -0400

    Updated docker compose with db migrations

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,7 +14,7 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
